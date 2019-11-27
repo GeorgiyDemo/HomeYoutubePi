8e679b85f57269ba1513aabb8e121c88e06277b5
Georgiy Demo <demenchuk.george@protonmail.com>
2019-11-27 09:51:03 +0300

    Реформат + комментарии

--- player/video_player.py
+++ player/video_player.py
@@ -1,13 +1,39 @@
 import subprocess
+import time
+
+import psutil
 from omxplayer import OMXPlayer
+
 
 class MainClass():
     def __init__(self, video_url):
+        self.process_name = "omxplayer"
         self.video_url = video_url
         self.processing()
-    
+
+    def checkIfProcessRunning(self):
+        """
+        Проверка на существование процесса
+        :return:
+        """
+        for proc in psutil.process_iter():
+            try:
+                if self.process_name.lower() in proc.name().lower():
+                    return True
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                pass
+        return False
+
     def processing(self):
-        
-        proc = subprocess.Popen(['youtube-dl','-f','best', '-g', self.video_url], stdout=subprocess.PIPE)
-        realurl=proc.stdout.read()
-        player = OMXPlayer(realurl.decode("utf-8", "strict")[:-1],args=['-o', 'local'])
+        """
+        - Получение потока воспроизведения с youtube-dl
+        - Запуск omxplayer
+        :return:
+        """
+        proc = subprocess.Popen(['youtube-dl', '-f', 'best', '-g', self.video_url], stdout=subprocess.PIPE)
+        realurl = proc.stdout.read()
+        player = OMXPlayer(realurl.decode("utf-8", "strict")[:-1], args=['-o', 'local'])
+
+        # Необходимо для удержания очереди в FIFO
+        while self.checkIfProcessRunning():
+            time.sleep(3)
--- telegram/tlg.py
+++ telegram/tlg.py
@@ -1,20 +1,19 @@
 """
-    Клиент Telegram для добавления плейлиста восроизведения на Raspberry Pi
+    Клиент Telegram для добавления плейлиста воспроизведения на Raspberry Pi
 """
 
-import rq
-import redis
-
-import telegram
 import time
-import yaml
 
+import redis
+import rq
+import telegram
+import yaml
 from telegram.error import NetworkError, Unauthorized
 
 
 class GetSettingsClass(object):
     """
-    Класс для чтения настроек с yaml
+    Чтение настроек с yaml
     """
 
     def __init__(self):
@@ -26,15 +25,18 @@ class GetSettingsClass(object):
 
 
 class TelegramCli(object):
+    """
+    Запуск Telegram-клиента
+    """
 
     def __init__(self, queue, token, proxy):
-        
+
         self.queue = queue
         request = telegram.utils.request.Request(proxy_url=proxy)
-        
+
         self.update_id = None
-        self.bot = telegram.Bot(token,request=request)
-        
+        self.bot = telegram.Bot(token, request=request)
+
         try:
             self.update_id = self.bot.get_updates()[0].update_id
         except IndexError:
@@ -48,7 +50,6 @@ class TelegramCli(object):
             except Unauthorized:
                 self.update_id += 1
 
-
     def handler(self):
 
         for update in self.bot.get_updates(offset=self.update_id, timeout=10):
@@ -57,16 +58,21 @@ class TelegramCli(object):
 
             if user_msg == "/start":
                 update.message.reply_text("Привет, добро пожаловать в коммуналку Демы\nСкинь ссылку на видос YouTube")
-                self.bot.sendPhoto(chat_id=update.message.chat.id, photo='https://sun9-37.userapi.com/c857624/v857624432/10708d/u7yl1BWKmDY.jpg')
-            
+                self.bot.sendPhoto(chat_id=update.message.chat.id,
+                                   photo='https://sun9-37.userapi.com/c857624/v857624432/10708d/u7yl1BWKmDY.jpg')
+
             elif "youtube.com" in user_msg or "youtu.be" in user_msg:
                 self.queue.enqueue('video_player.MainClass', user_msg)
-                 
+                update.message.reply_text("Добавили видео в очередь 😉")
+
 
 def main():
+    # Подключение очереди rq
     queue = rq.Queue('youtube', connection=redis.Redis.from_url('redis://redis:6379/0'))
+    # Получаем настройки бота
     obj = GetSettingsClass()
-    TelegramCli(queue, obj.c["telegram_token"],obj.c["proxy_str"])
+    # Запускаем клиент
+    TelegramCli(queue, obj.c["telegram_token"], obj.c["proxy_str"])
 
 
 if __name__ == '__main__':
